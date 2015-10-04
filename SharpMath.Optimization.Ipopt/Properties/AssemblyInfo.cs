4a4de933ad1b09b23d2fdb9d55b8ac188e1de112
Morten Bakkedal <bakkedal@gmail.com>
2015-10-04 10:44:58 +0200

    Added SharpMath.Optimization.Ipopt project.

--- SharpMath.Optimization.Ipopt/Properties/AssemblyInfo.cs
+++ SharpMath.Optimization.Ipopt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpMath.Optimization")]
+[assembly: AssemblyTitle("SharpMath.Optimization.Ipopt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpMath.Optimization")]
+[assembly: AssemblyProduct("SharpMath.Optimization.Ipopt")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4aae9ea7-415b-4326-a2fb-702a17df03f5")]
+[assembly: Guid("bb89996e-8802-46d1-b16a-d8a55dd64176")]
 
 // Version information for an assembly consists of the following four values:
 //
