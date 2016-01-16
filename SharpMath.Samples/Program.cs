700126881df764d0eb62c893aeba97da50655734
Morten Bakkedal <bakkedal@gmail.com>
2016-01-16 15:57:20 +0100

    Cleanup.

--- SharpMath.Samples/Program.cs
+++ SharpMath.Samples/Program.cs
@@ -1,11 +1,8 @@
 ﻿// SharpMath - C# Mathematical Library
-// Copyright (c) 2015 Morten Bakkedal
+// Copyright (c) 2016 Morten Bakkedal
 // This code is published under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 using SharpMath.Samples.Optimization;
 
@@ -15,9 +12,6 @@ namespace SharpMath.Samples
 	{
 		public static void Main(string[] args)
 		{
-			//Scan(new DirectoryInfo(@"D:\Udvikling\SharpMath\SharpMath")); return;
-
-			//ImportIpopt.Import();
 			OptimizerSamples.Sample2();
 
 			if (IntPtr.Size == 4)
@@ -35,45 +29,6 @@ namespace SharpMath.Samples
 
 			Console.WriteLine("Done");
 			Console.ReadKey();
-		}
-
-		private static void Scan(DirectoryInfo d0)
-		{
-			foreach (DirectoryInfo d in d0.GetDirectories())
-			{
-				Scan(d);
-			}
-
-			foreach (FileInfo f in d0.GetFiles().Where(f2 => f2.Name.EndsWith(".cs")))
-			{
-				if (!f.FullName.EndsWith(".cs"))
-				{
-					continue;
-				}
-				List<string> lines = File.ReadAllLines(f.FullName).ToList();
-				if (lines.Count == 0)
-				{
-					continue;
-				}
-
-				if (lines[0].StartsWith("using "))
-				{
-					lines.Insert(0, "");
-					lines.Insert(0, "// This code is published under the MIT License.");
-					lines.Insert(0, "// Copyright (c) 2016 Morten Bakkedal");
-				}
-				if (!lines[0].Contains("SharpMath - C#"))
-				{
-					lines.Insert(0, "");
-				}
-				else
-				{
-				}
-
-				lines[0] = "// SharpMath - C# Mathematical Library";
-
-				File.WriteAllLines(f.FullName, lines.ToArray());
-			}
 		}
 	}
 }
